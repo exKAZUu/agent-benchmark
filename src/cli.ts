75638fe684b8af89a71c943070c0dbfc9565afe9
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-12-30 14:23:30 +0900

    feat: employ yargs

---
+++ src/cli.ts
@@ -0,0 +1,16 @@
+export const currentText = "Hello via Bun!";
+
+export type Operator = "+" | "-" | "*" | "/";
+
+export function calculate(left: number, operator: Operator, right: number): number {
+  switch (operator) {
+    case "+":
+      return left + right;
+    case "-":
+      return left - right;
+    case "*":
+      return left * right;
+    case "/":
+      return left / right;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { calculate, currentText } from "./cli";
+
+yargs(hideBin(process.argv))
+  .command(
+    "hello",
+    "Print the current text",
+    () => {},
+    () => {
+      console.log(currentText);
+    },
+  )
+  .command(
+    "calc <left> <operator> <right>",
+    "Calculate a result from two numbers and an operator",
+    (cmd) =>
+      cmd
+        .positional("left", {
+          type: "number",
+          demandOption: true,
+        })
+        .positional("operator", {
+          type: "string",
+          choices: ["+", "-", "*", "/"] as const,
+          demandOption: true,
+        })
+        .positional("right", {
+          type: "number",
+          demandOption: true,
+        }),
+    (argv) => {
+      const left = argv.left as number;
+      const right = argv.right as number;
+      const operator = argv.operator as "+" | "-" | "*" | "/";
+
+      console.log(calculate(left, operator, right));
+    },
+  )
+  .demandCommand(1)
+  .strict()
+  .help()
+  .parse();
