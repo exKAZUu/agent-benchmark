7a36cc816ea66bbf8b1bb6a4e09ce28330212769
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-01-10 11:06:45 +0900

    feat: support modulo operator

--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -17,4 +17,9 @@ describe("calculate", () => {
   test("divides", () => {
     expect(calculate(12, "/", 3)).toBe(4);
   });
+
+  test("modulo", () => {
+    expect(calculate(10, "%", 3)).toBe(1);
+    expect(calculate(10, "%", 5)).toBe(0);
+  });
 });
