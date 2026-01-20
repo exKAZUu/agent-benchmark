43b2e16b814875be3ed534f2096ab241a58bc8bc
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-01-20 21:10:38 +0900

    feat: Employ Commander instead of Yargs
    Co-authored-by: DuelingAgents-Bot <bot@dueling-agents.com>

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,32 @@
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import { Command, InvalidArgumentError } from "commander";
 import { calculate, currentText } from "./cli";
 
-yargs(hideBin(process.argv))
-  .command(
-    "hello",
-    "Print the current text",
-    () => {},
-    () => {
-      console.log(currentText);
-    },
-  )
-  .command(
-    "calc <left> <operator> <right>",
-    "Calculate a result from two numbers and an operator",
-    (cmd) =>
-      cmd
-        .positional("left", {
-          type: "number",
-          demandOption: true,
-        })
-        .positional("operator", {
-          type: "string",
-          choices: ["+", "-", "*", "/"] as const,
-          demandOption: true,
-        })
-        .positional("right", {
-          type: "number",
-          demandOption: true,
-        }),
-    (argv) => {
-      const left = argv.left as number;
-      const right = argv.right as number;
-      const operator = argv.operator as "+" | "-" | "*" | "/";
+const program = new Command();
 
-      console.log(calculate(left, operator, right));
+program
+  .command("hello")
+  .description("Print the current text")
+  .action(() => {
+    console.log(currentText);
+  });
+
+program
+  .command("calc")
+  .description("Calculate a result from two numbers and an operator")
+  .argument("<left>", "the left-hand side of the calculation", parseFloat)
+  .argument(
+    "<operator>",
+    "the operator to use in the calculation",
+    (value) => {
+      if (!["+", "-", "*", "/"].includes(value)) {
+        throw new InvalidArgumentError("Operator must be one of +, -, *, /");
+      }
+      return value as "+" | "-" | "*" | "/";
     },
   )
-  .demandCommand(1)
-  .strict()
-  .help()
-  .parse();
+  .argument("<right>", "the right-hand side of the calculation", parseFloat)
+  .action((left, operator, right) => {
+    console.log(calculate(left, operator, right));
+  });
+
+program.parse(process.argv);
